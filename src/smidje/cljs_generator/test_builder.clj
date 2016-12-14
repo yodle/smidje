2d6534a3f3873428f12e3f7ba9a2e3835291bebd
ragnorak1000 <ragnorak1000@gmail.com>
2016-12-14 11:36:35 -0500

    adding basic generator

--- src/smidje/cljs_generator/test_builder.clj
+++ src/smidje/cljs_generator/test_builder.clj
@@ -1,4 +1,17 @@
-(ns smidje.cljs-generator.test-builder)
+(ns smidje.cljs-generator.test-builder
+  (:require [clojure.test :refer :all]))
 
-(defn generate-tests [testmap]
-  ())
+(defn generate-assertion [assertion]
+  (let [test-function#   (:function-under-test assertion)
+        expected-result# (:expected-result assertion)]
+    `(is (= ~test-function# ~expected-result#))))
+
+(defn generate-test [test-definition]
+  (let [assertions# (:assertions test-definition)
+        name#       (:name test-definition)]
+    `(deftest ~(symbol name#)
+       ~@(map generate-assertion assertions#))))
+
+(defn generate-tests [test-runtime]
+  (let [tests# (:tests test-runtime)]
+    `(do ~@(map generate-test tests#))))
