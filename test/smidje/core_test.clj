076e871303fe457060c8c580cbbeea95db3ab15d
Dennis Lipovsky <dlipovsky@yodle.com>
2016-12-14 10:59:23 -0500

    some simple tests for the cljs-test generator

--- test/smidje/core_test.clj
+++ test/smidje/core_test.clj
@@ -1,7 +1,3 @@
 (ns smidje.core-test
-  (:require [clojure.test :refer :all]
+  (:require [midje.sweet :refer :all]
             [smidje.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
