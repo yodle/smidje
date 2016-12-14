dc55e27f1dd6bb63677ace31ecba6b86851cbae7
Luke M. Elliot <luke.elliot@yodle.com>
2016-12-14 15:08:03 -0500

    updated test so it strips out the fn name

--- test/smidje/parser_test.clj
+++ test/smidje/parser_test.clj
@@ -4,11 +4,10 @@
             [smidje.parser :as parser :refer :all]))
 
 
+; parse is called from fact function, after it pops off the name.
 ; given the fact, call parse with it and get the map
-
 (def simple-addition-fact
-  '(fact "simple addition"
-         (+ 1 1 => 2)))
+  '((+ 1 1) => 2))
 
 (m/fact "simple addition"
       (parser/parse simple-addition-fact) => im/simple-addition-assertion)
