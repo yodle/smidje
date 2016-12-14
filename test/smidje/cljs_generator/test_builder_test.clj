fe7f95f8d2be813440474878bf0881208e3ff698
Dennis Lipovsky <dennis.lipovsky@gmail.com>
2016-12-14 18:15:52 -0500

    writing the macro tests for =not=> arrow

--- test/smidje/cljs_generator/test_builder_test.clj
+++ test/smidje/cljs_generator/test_builder_test.clj
@@ -19,3 +19,11 @@
 
 (fact "multiple assertion fact"
       (generate-tests im/multiple-expect-match-map) => multiple-expect-match-cljs)
+
+(def single-expect-unequal-cljs
+  `(do
+     (cljs.test/deftest ~(symbol "addition is well defined")
+       (cljs.test/is (cljs.core/not= (~'+ 1 1) 3)))))
+
+(fact "not assertion fact"
+      (generate-tests im/single-expect-unequal-map) => single-expect-unequal-cljs)
