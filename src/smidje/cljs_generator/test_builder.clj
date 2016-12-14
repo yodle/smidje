ddaee33200db9136037288bb1b01d78725bd03d7
Russell John-Baptiste <russell.john.baptiste@gmail.com>
2016-12-14 15:47:38 -0500

    cljs.core/do --> do

--- src/smidje/cljs_generator/test_builder.clj
+++ src/smidje/cljs_generator/test_builder.clj
@@ -13,7 +13,7 @@
 
 (defn generate-tests [test-runtime]
   (let [tests# (:tests test-runtime)]
-     `(cljs.core/do ~@(map generate-test tests#))))
+     `(do ~@(map generate-test tests#))))
 
 (defmacro testmacro [test-runtime]
   (generate-tests test-runtime))
