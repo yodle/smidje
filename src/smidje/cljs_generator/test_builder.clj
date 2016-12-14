28c310f09f4747dccae57ba846ea5dd0daaf42ef
Ruslan Fridman <rfridman@yodle.com>
2016-12-14 16:17:07 -0500

    integrating parser and cljs generator

--- src/smidje/cljs_generator/test_builder.clj
+++ src/smidje/cljs_generator/test_builder.clj
@@ -1,7 +1,7 @@
 (ns smidje.cljs-generator.test-builder)
 
 (defn generate-assertion [assertion]
-  (let [test-function#   (:function-under-test assertion)
+  (let [test-function#   (:call-form assertion)
         expected-result# (:expected-result assertion)]
     `(cljs.test/is (cljs.core/= ~test-function# ~expected-result#))))
 
@@ -13,7 +13,7 @@
 
 (defn generate-tests [test-runtime]
   (let [tests# (:tests test-runtime)]
-     `(cljs.core/do ~@(map generate-test tests#))))
+     `(do ~@(map generate-test tests#))))
 
 (defmacro testmacro [test-runtime]
   (generate-tests test-runtime))
--- test/smidje/cljs/macro_test.cljs
+++ test/smidje/cljs/macro_test.cljs
@@ -1,5 +1,10 @@
 (ns smidje.cljs.macro-test
     (:require [cljs.test :refer [deftest is]])
-    (:require-macros [smidje.cljs-generator.test-builder :refer [testmacro]]))
+    (:require-macros [smidje.parser :refer [fact]]
+                     [smidje.cljs-generator.test-builder :refer [testmacro]]))
 
-(testmacro {:tests [{:name "mytest", :assertions [{:function-under-test (+ 1 1), :expected-result 2}]}]})
+;(testmacro {:tests [{:name "mytest", :assertions [{:function-under-test (+ 1 1), :expected-result 2}]}]})
+
+(fact "name"
+  (+ 1 1) => 2
+      (+ 1 2) =not=> 5)
