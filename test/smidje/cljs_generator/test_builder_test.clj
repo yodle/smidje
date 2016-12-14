dc31e9f100c722ad84161b7a100483e072b802c4
Dennis Lipovsky <dlipovsky@yodle.com>
2016-12-14 11:27:04 -0500

    refactoring intermediate maps into a separate test source file

--- test/smidje/cljs_generator/test_builder_test.clj
+++ test/smidje/cljs_generator/test_builder_test.clj
@@ -1,48 +1,14 @@
 (ns smidje.cljs-generator.test-builder-test
   (:require [midje.sweet :refer :all]
+            [smidje.intermediate-maps :as im]
             [smidje.cljs-generator.test-builder :refer :all]))
-
-(def test-metadata
-  {:smidje/namespace 'smidje.core-test
-   :smidje/file ""})
-
-(def simple-addition-assertion
-  {:expected-result-form '2
-   :expected-result 2
-   :arrow '=>
-   :call-form '(+ 1 1)
-   :function-under-test (clojure.core/fn [] (+ 1 1))
-   :position 1})
-
-(def ternary-addition-assertion
-  {:expected-result-form '3
-   :expected-result 3
-   :arrow '=>
-   :call-form '(+ 1 1 1)
-   :function-under-test (clojure.core/fn [] (+ 1 1 1))
-   :position 2})
-
-(def single-expect-match-map
-  (merge
-    test-metadata
-    {:smidje/source '(fact (+ 1 1) => 2)
-    :tests `[:description "addition is simple"
-             :assertions [~simple-addition-assertion]]}))
 
 (def single-expect-match-cljs
   '(cljs.test/deftest
      (cljs.test/is (clojure.core/= (clojure.core/+ 1 1) 2))))
 
 (fact "a single expect match arrow fact"
-      (generate-tests single-expect-match-map) => single-expect-match-cljs)
-
-(def multiple-expect-match-map
-  (merge
-    test-metadata
-    {:smidje/source '(fact (+ 1 1) => 2 (+ 1 1 1) => 3)
-    :tests `[:description "more addition testing"
-             :assertions [~simple-addition-assertion
-                          ~ternary-addition-assertion]]}))
+      (generate-tests im/single-expect-match-map) => single-expect-match-cljs)
 
 (def multiple-expect-match-cljs
   '(cljs.test/deftest
@@ -50,8 +16,7 @@
      (cljs.test/is (clojure.core/= (clojure.core (+ 1 1 1)) 3))))
 
 (fact "multiple assertion fact"
-      (generate-tests multiple-expect-match-map) => multiple-expect-match-cljs)
-
+      (generate-tests im/multiple-expect-match-map) => multiple-expect-match-cljs)
 
 (comment
 
