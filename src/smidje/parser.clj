b7b4a69de4a9782ccf913c6f389de978d9a61687
Dustin Vogt <dustin.vogt@yodle.com>
2016-12-15 10:35:16 -0500

    experimental parse

--- src/smidje/parser.clj
+++ src/smidje/parser.clj
@@ -21,27 +21,37 @@
       {:provided (nth forms 3)}
       {}))
 
+(defn throws-form?
+  [form]
+  (= (first form) 'throws))
+
+(defn- parse-expected
+  [form]
+  (if (throws-form? form)
+    (merge
+      {:throws-exception (second form)}
+      (when (> (count form) 2)
+        {:throws-message (nth form 2)}))
+    {:expected-result form}))
+
 (defn- parse-equals
   [forms]
   (let [call-form (nth forms 0)
         arrow (nth forms 1)
         expected-form (nth forms 2)]
     (merge
-    {:call-form            call-form
-     :arrow                arrow
-     :expected-result      expected-form
-     :expected-result-form `'~expected-form}
-     (parse-provided forms))))
+      {:call-form            call-form
+       :arrow                arrow
+       ;:expected-result      expected-form
+       :expected-result-form `'~expected-form}
+      (parse-expected expected-form)
+      (parse-provided forms))))
 
 
 (defn- is-arrow
   [form]
   (or (= form '=>)
       (= form '=not=>)))
-
-(defn throws-form?
-  [form]
-  (= (first form) 'throws))
 
 (defn parse
   [forms]
