a55c526880be22711743b031e86a2d789ae3cb78
ragnorak1000 <ragnorak1000@gmail.com>
2016-12-15 18:25:33 -0500

    adding swap to add calls

--- src/smidje/cljs_generator/mocks.clj
+++ src/smidje/cljs_generator/mocks.clj
@@ -8,9 +8,8 @@
   [function mock-config-atom]
   `(cljs.core/fn [& params#]
      (let [mock-config# (cljs.core/get-in (cljs.core/deref ~mock-config-atom) [~function :mock-config])]
-       ;(cljs.core/swap! ~mock-config-atom
-       ;                 (cljs.core/fn[value#]
-       ;                   (cljs.core/update value# :calls #(cljs.core/merge-with cljs.core/+ % {params# 1}))))
+       (cljs.core/swap! ~mock-config-atom cljs.core/update-in [~function :calls params#] (cljs.core/fnil cljs.core/inc 0))
+       (cljs.core/println (cljs.core/deref ~mock-config-atom))
        (if
          (cljs.core/contains? mock-config# params#)
          (cljs.core/get-in mock-config# [params# :result])
