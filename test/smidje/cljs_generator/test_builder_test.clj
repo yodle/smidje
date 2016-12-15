541d4ef759bca441d87f6fa0bdebb5751a033170
Dennis Lipovsky <dennis.lipovsky@gmail.com>
2016-12-15 13:29:56 -0500

    fixing the unit tests for generate-tests adding some lower level unit tests

--- test/smidje/cljs_generator/test_builder_test.clj
+++ test/smidje/cljs_generator/test_builder_test.clj
@@ -6,7 +6,8 @@
 (def single-expect-match-cljs
   `(do
      (cljs.test/deftest ~(symbol "addition is simple")
-       (cljs.test/is (cljs.core/= (~'+ 1 1) 2)))))
+       (cljs.core/with-redefs []
+         (cljs.test/is (cljs.core/= (~'+ 1 1) 2))))))
 
 (fact "a single expect match arrow fact"
       (generate-tests im/single-expect-match-map) => single-expect-match-cljs)
@@ -14,8 +15,10 @@
 (def multiple-expect-match-cljs
   `(do
      (cljs.test/deftest ~(symbol "more addition testing")
-       (cljs.test/is (cljs.core/= (~'+ 1 1) 2))
-       (cljs.test/is (cljs.core/= (~'+ 1 1 1) 3)))))
+       (cljs.core/with-redefs []
+         (cljs.test/is (cljs.core/= (~'+ 1 1) 2)))
+       (cljs.core/with-redefs []
+         (cljs.test/is (cljs.core/= (~'+ 1 1 1) 3))))))
 
 (fact "multiple assertion fact"
       (generate-tests im/multiple-expect-match-map) => multiple-expect-match-cljs)
@@ -23,7 +26,17 @@
 (def single-expect-unequal-cljs
   `(do
      (cljs.test/deftest ~(symbol "addition is well defined")
-       (cljs.test/is (cljs.core/not= (~'+ 1 1) 3)))))
+       (cljs.core/with-redefs []
+         (cljs.test/is (cljs.core/not= (~'+ 1 1) 3))))))
 
 (fact "not assertion fact"
       (generate-tests im/single-expect-unequal-map) => single-expect-unequal-cljs)
+
+(tabular
+ (fact "arrows generate correct equality checks"
+       (do-arrow ?arrow) => ?expected)
+ ?arrow     ?expected
+ '=>        'cljs.core/=
+ '=not=>    'cljs.core/not=
+ '=not>     'cljs.core/not=
+ '=bogus=>  (throws Exception))
