0a5fa1ffec794c41b3a7bc025f900881d322fc2d
Dennis Lipovsky <dennis.lipovsky@gmail.com>
2016-12-16 10:09:38 -0500

    fixing the midje tests for the generator

--- test/smidje/cljs_generator/test_builder_test.clj
+++ test/smidje/cljs_generator/test_builder_test.clj
@@ -3,34 +3,13 @@
             [smidje.intermediate-maps :as im]
             [smidje.cljs-generator.test-builder :refer :all]))
 
-(def single-expect-match-cljs
-  `(do
-     (cljs.test/deftest ~(symbol "addition is simple")
-       (cljs.core/with-redefs []
-         (cljs.test/is (cljs.core/= (~'+ 1 1) 2))))))
-
 (fact "a single expect match arrow fact"
-      (generate-tests im/single-expect-match-map) => single-expect-match-cljs)
+      (generate-single-assert im/simple-addition-assertion)
+      => `(cljs.test/is (cljs.core/= (~'+ 1 1) 2)))
 
-(def multiple-expect-match-cljs
-  `(do
-     (cljs.test/deftest ~(symbol "more addition testing")
-       (cljs.core/with-redefs []
-         (cljs.test/is (cljs.core/= (~'+ 1 1) 2)))
-       (cljs.core/with-redefs []
-         (cljs.test/is (cljs.core/= (~'+ 1 1 1) 3))))))
-
-(fact "multiple assertion fact"
-      (generate-tests im/multiple-expect-match-map) => multiple-expect-match-cljs)
-
-(def single-expect-unequal-cljs
-  `(do
-     (cljs.test/deftest ~(symbol "addition is well defined")
-       (cljs.core/with-redefs []
-         (cljs.test/is (cljs.core/not= (~'+ 1 1) 3))))))
-
-(fact "not assertion fact"
-      (generate-tests im/single-expect-unequal-map) => single-expect-unequal-cljs)
+(fact "a single not assertion fact"
+      (generate-single-assert im/simple-addtion-not-assertion)
+      => `(cljs.test/is (cljs.core/not= (~'+ 1 1) 3)))
 
 (tabular
  (fact "arrows generate correct equality checks"
