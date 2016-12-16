f75f29d21976cc8de1d6bd7f37fbc3cc14bc7997
Dennis Lipovsky <dlipovsky@yodle.com>
2016-12-16 13:24:38 -0500

    fixing the generated tabular form

--- src/smidje/parser.clj
+++ src/smidje/parser.clj
@@ -177,7 +177,7 @@
                 _ (println "fact-table-values" fact-table-values)                    ; (1 2 3 3 4 7 9 10 19)
                 facts (gen-facts fact-template fact-table-binding fact-table-values)
                 _ (println "facts" facts)]
-               `(fact ~fact-name ~@facts)))
+               (concat `(fact ~fact-name) (apply concat facts))))
 
 (defmacro fact
   [& _]
--- test/smidje/cljs/macro_test.cljs
+++ test/smidje/cljs/macro_test.cljs
@@ -5,15 +5,15 @@
 
 ;(testmacro {:tests [{:name "mytest", :assertions [{:function-under-test (+ 1 1), :expected-result 2}]}]})
 
-;(fact "name"
-;  (+ 1 1) => 2
-;  (+ 1 3) =not=> 2
-;  (+ 1 3) =not> 2)
+(fact "name"
+  (+ 1 1) => 2
+  (+ 1 3) =not=> 2
+  (+ 1 3) =not> 2)
 
-(tabular "tabularname"
-         (fact "factname"
-               (+ ?a ?b) => ?c)
-         ?a ?b ?c
-         1 2 3
-         3 4 7
-         9 10 19)
+(println (macroexpand-1 '(tabular "tabularname"
+                                  (fact "factname"
+                                        (+ ?a ?b) => ?c)
+                                  ?a ?b ?c
+                                  1 2 3
+                                  3 4 7
+                                  9 10 19)))
