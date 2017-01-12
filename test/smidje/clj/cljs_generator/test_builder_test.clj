58c7f2a474831a805d9a794f4565e0fc8a07340b
rfridman <rfridman@yodle.com>
2017-01-12 13:29:34 -0500

    fixing issue with function/multi-method mismatch on mocking report

--- test/smidje/clj/cljs_generator/test_builder_test.clj
+++ test/smidje/clj/cljs_generator/test_builder_test.clj
@@ -3,40 +3,43 @@
             [smidje.clj.intermediate-maps :as im]
             [smidje.cljs-generator.test-builder :refer :all]))
 
-(background (clojure.test/report anything) => {})
+(defmulti new-report (fn [& args] :default))
+(defmethod new-report :default [& args] {})
 
-(facts
-  "generate-single-assert with normal arrow"
-  (fact
-    "successful assertion with constant succeeds"
-    (eval (generate-single-assert im/simple-successful-addition-assertion)) => true)
-  (fact
-    "failed assertion with constant fails"
-    (eval (generate-single-assert im/simple-failing-addition-assertion)) => false)
-  (fact
-    "successful assertion with checker function succeeds"
-    (eval (generate-single-assert im/simple-successful-checker-function-assertion)) => true)
-  (fact
-    "failed assertion with checker function fails"
-    (eval (generate-single-assert im/simple-failed-checker-function-assertion)) => false))
+(with-redefs [clojure.test/report new-report]               ;required to suppress clojure.test output from methods under test
 
-(facts
-  "generate-single-assert with not arrow"
-  (fact
-    "successful assertion with constant succeeds"
-    (eval (generate-single-assert im/simple-addition-successful-not-assertion)) => true)
-  (fact
-    "failed assertion with constant fails"
-    (eval (generate-single-assert im/simple-addition-failed-not-assertion)) => false))
+  (facts
+    "generate-single-assert with normal arrow"
+    (fact
+      "successful assertion with constant succeeds"
+      (eval (generate-single-assert im/simple-successful-addition-assertion)) => true)
+    (fact
+      "failed assertion with constant fails"
+      (eval (generate-single-assert im/simple-failing-addition-assertion)) => false)
+    (fact
+      "successful assertion with checker function succeeds"
+      (eval (generate-single-assert im/simple-successful-checker-function-assertion)) => true)
+    (fact
+      "failed assertion with checker function fails"
+      (eval (generate-single-assert im/simple-failed-checker-function-assertion)) => false))
 
-(fact "simple throws form correct"
-      (generate-expected-exception im/expected-exception-assertion)
-      => '(clojure.test/is (thrown? Exception (smidje.clj.intermediate-maps/foo nil))))
+  (facts
+    "generate-single-assert with not arrow"
+    (fact
+      "successful assertion with constant succeeds"
+      (eval (generate-single-assert im/simple-addition-successful-not-assertion)) => true)
+    (fact
+      "failed assertion with constant fails"
+      (eval (generate-single-assert im/simple-addition-failed-not-assertion)) => false))
 
-(tabular
- (fact "arrows generate correct equality checks"
-       (do-arrow ?arrow) => ?expected)
- ?arrow     ?expected
- '=>        '=
- '=not=>    'not=
- '=bogus=>  (throws Exception))
+  (fact "simple throws form correct"
+        (generate-expected-exception im/expected-exception-assertion)
+        => '(clojure.test/is (thrown? Exception (smidje.clj.intermediate-maps/foo nil))))
+
+  (tabular
+    (fact "arrows generate correct equality checks"
+          (do-arrow ?arrow) => ?expected)
+    ?arrow ?expected
+    '=> '=
+    '=not=> 'not=
+    '=bogus=> (throws Exception)))
