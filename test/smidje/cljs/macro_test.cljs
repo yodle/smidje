eedb74ff470a903a3f2848b5ad501bd36b8489ed
Dustin Vogt <dustin.vogt@yodle.com>
2017-02-23 12:46:03 -0500

    Adding :times support

--- test/smidje/cljs/macro_test.cljs
+++ test/smidje/cljs/macro_test.cljs
@@ -12,6 +12,13 @@
 (defn foo []
   (+ (bar) (thing 1)))
 
+(defn two-bars []
+  (+ (bar) (bar)))
+
+(defn n-bars [times-called]
+  (dotimes [n times-called]
+    (bar)))
+
 (fact "multi-provided"
       (foo) => 2
       (provided
@@ -66,6 +73,20 @@
   (thing 1) =not=> ..result..
   (provided
     (thing 1) => ..badresult..))
+
+(fact
+  "times checking"
+  (two-bars) => 4
+  (provided
+    (bar) => 2 :times 2
+    (foo) => ..whatever.. :times 0
+    (thing "foo") => ..no1cur.. :times (range)))
+
+(fact
+  "more times checking"
+  (n-bars 5) => nil?
+  (provided
+    (bar) => 2 :times (range 5)))
 
 (fact
   "expects exception when thrown by provided"
